f79b9db2e470140821a56b05b7fffadfe7027b79
Herb Miller Jr <hmillerj@gmail.com>
2020-08-15 08:55:35 -0400

    Remove outdated kernels during clean operation

--- maint.py
+++ maint.py
@@ -3,11 +3,11 @@
 import argparse
 import glob
 import os
+import shutil
 import subprocess
 import yaml
 
 from distutils.version import LooseVersion
-from shutil import copyfile
 from termcolor import colored
 
 parser=argparse.ArgumentParser(description='Perform common Gentoo maintenance tasks')
@@ -25,6 +25,47 @@ with open("/usr/local/etc/{0}.yaml".format(os.path.splitext(os.path.basename(__f
 
 def inform(command):
 	print("{0} {1}".format(colored("Executing:","white",attrs=['bold']),command))
+
+def to_kernel_version(package):
+	segments=package.split("-")
+	if len(segments) < 3:
+		raise ValueError("Invalid kernel package name encountered")
+	package=segments[1]
+	if len(segments) > 3:
+		package+="-"+segments[3]
+	return package
+
+def to_package_version(version):
+	version=version.split("-")
+	package="linux-{0}-gentoo".format(version[0])
+	if len(version) > 1:
+		package+="-"+version[1]
+	return package
+
+def available_kernels():
+	return subprocess.run(["eselect","kernel","list"],stdout=subprocess.PIPE,universal_newlines=True).stdout.split("\n")
+
+def remove_outdated_kernels():
+	available=[version.split(" ")[5] for version in available_kernels() if "linux" in version]
+	remaining=[path for path in glob.glob("/usr/src/linux-*-gentoo*") if os.path.basename(path) not in available]
+	for kernel in remaining:
+		print("{0} {1}".format(colored("Removing:","white",attrs=['bold']),os.path.basename(kernel)))
+		print(kernel)
+		shutil.rmtree(kernel)
+		kernel=os.path.basename(kernel)
+		for filetype in ["config","System.map","vmlinuz"]:
+			path="{0}/{1}-{2}".format(global_configuration['boot']['path'],filetype,kernel)
+			if 'kernel_suffix' in global_configuration:
+				path+="-"+global_configuration['kernel_suffix']
+			print(path)
+			if os.path.exists(path):
+				os.remove(path)
+		path="/lib/modules/{0}".format(kernel)
+		if 'kernel_suffix' in global_configuration:
+			path+="-"+global_configuration['kernel_suffix']
+		print(path)
+		if os.path.exists(path):
+			shutil.rmtree(path)
 
 if global_arguments.upgrade:
 	command=["emerge","-uDNa","--keep-going"]
@@ -51,30 +92,15 @@ if global_arguments.special:
 
 if global_arguments.clean:
 	subprocess.run(["emerge","-a","--depclean"])
+	remove_outdated_kernels()
 	subprocess.run(["emerge","-a","@preserved-rebuild"])
 	subprocess.run(["revdep-rebuild"])
 	subprocess.run(["etc-update"])
 	subprocess.run(["haskell-updater","-uv"])
 	subprocess.run(["eselect","news","read"])
 
-def to_kernel_version(package):
-	segments=package.split("-")
-	if len(segments) < 3:
-		raise ValueError("Invalid kernel package name encountered")
-	package=segments[1]
-	if len(segments) > 3:
-		package+="-"+segments[3]
-	return package
-
-def to_package_version(version):
-	version=version.split("-")
-	package="linux-{0}-gentoo".format(version[0])
-	if len(version) > 1:
-		package+="-"+version[1]
-	return package
-
 if global_arguments.kernel:
-	output=subprocess.run(["eselect","kernel","list"],stdout=subprocess.PIPE,universal_newlines=True).stdout.split("\n")
+	output=available_kernels()
 	current=max([to_kernel_version(version.split(" ")[5]) for version in output if "*" in version],key=LooseVersion)
 	available=[to_kernel_version(version.split(" ")[5]) for version in output if "linux" in version]
 	latest=max(available,key=LooseVersion)
@@ -84,7 +110,7 @@ if global_arguments.kernel:
 	command=["eselect","kernel","set",to_package_version(latest)]
 	inform(command)
 	subprocess.run(command)
-	copyfile("/usr/src/{0}/.config".format(to_package_version(current)),"/usr/src/linux/.config")
+	shutil.copyfile("/usr/src/{0}/.config".format(to_package_version(current)),"/usr/src/linux/.config")
 	working_directory="/usr/src/linux"
 	commands=[
 		["make","olddefconfig"],
